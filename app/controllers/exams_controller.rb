cfd5d4a9d4e44df354f171a5bba2cc08cc909f44
masifnafiz <masifnafiz@cps.edu>
2016-04-26 13:24:48 +0000

    first comment

---
+++ app/controllers/exams_controller.rb
@@ -0,0 +1,14 @@
+class ExamsController < ApplicationController
+    def main
+        render "main"
+    end
+    def personal
+        render "personal"
+    end
+    def sports
+        render "sports"
+    end
+    def hobbies
+        render "hobbies"
+    end
+end
